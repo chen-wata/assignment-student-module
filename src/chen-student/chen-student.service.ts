f347f19e0afede44887c90467c1b783c738af837
chen-wata <tussichenph@gmail.com>
2025-01-11 13:30:46 +0800

    read

--- src/chen-student/chen-student.service.ts
+++ src/chen-student/chen-student.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Student } from './student.entity';
@@ -13,5 +13,18 @@ export class ChenStudentService {
     async createStudent(date: Partial<Student>): Promise<Student> {
         const student = this.studentRepository.create(date);
         return this.studentRepository.save(student);
+    }
+
+    async getStudents(): Promise<Student[]> {
+        return this.studentRepository.find();
+    }
+
+    async getStudentById(id: number): Promise<Student> {
+        const student = await this.studentRepository.findOneBy({ id });
+        if (!student) {
+            throw new NotFoundException(`Student with ID ${id} not found`);
+        }
+
+        return student;
     }
 }
